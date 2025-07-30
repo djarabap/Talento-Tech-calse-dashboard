5e0e2a8ad3b3cd8e6e97b3c9daaa39ab8d3c8799
David Jaraba <140478716+djarabap@users.noreply.github.com>
2025-07-30 18:56:35 -0500

    tablero1.py

--- tablero1.py
+++ tablero1.py
@@ -44,16 +44,6 @@ with steps[0]:
 #with steps[1]:
     #id st.button('podemos usar botones', type='primary')
 
-with steps [1]:
-    df=pd.read_csv("https://raw.githubusercontent.com/diplomado-bigdata-machinelearning-udea/Curso1/master/s03/dataVentas2009.csv")
-    df.Fecha=pd.to_datetime(df.Fecha, format='%d/%m/%Y')
-    df.set_index('Fecha',inplace=True)
-    # st.table(df)
-    varx=st.selectbox('Escoge la variable x', df.columns)
-    #vary=st.selectbox('Escoge la variable y',met_df['Conversiones'])
-    fig, ax = plt.subplots()
-    ax = sns.histplot(data=df, x=varx)
-    st.pyplot(fig)
 
 
 
