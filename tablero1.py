3273ae6e8d9240f9312381fd0884618cf8ec5b09
David Jaraba <140478716+djarabap@users.noreply.github.com>
2025-07-30 18:57:38 -0500

    tablero1.py

--- tablero1.py
+++ tablero1.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 st.set_page_config(layout='centered', 
                     page_title='Talento Tech',
