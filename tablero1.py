db8ad45da6b925e140b09a0d7df1311074ab1cbf
David Jaraba <140478716+djarabap@users.noreply.github.com>
2025-07-30 19:12:24 -0500

    tablero1.py

--- tablero1.py
+++ tablero1.py
@@ -4,7 +4,6 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 st.set_page_config(layout='centered', 
                     page_title='Talento Tech',
                     page_icon = ':smile:')
@@ -44,6 +43,7 @@ with steps[0]:
               delta_color='inverse')
 #with steps[1]:
     #id st.button('podemos usar botones', type='primary')
+
 with steps [1]:
     df=pd.read_csv("https://raw.githubusercontent.com/diplomado-bigdata-machinelearning-udea/Curso1/master/s03/dataVentas2009.csv")
     df.Fecha=pd.to_datetime(df.Fecha, format='%d/%m/%Y')
@@ -54,8 +54,3 @@ with steps [1]:
     fig, ax = plt.subplots()
     ax = sns.histplot(data=df, x=varx)
     st.pyplot(fig)
-
-
-
-
-
